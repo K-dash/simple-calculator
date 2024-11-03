432115e914c265bb66b0395c2dea3c0a9f927de3
K <51281148+K-dash@users.noreply.github.com>
2024-11-03 19:35:55 +0900

    feat: enhance memory handling with named slots
    メモリ管理をインデックスベースから名前ベースのスロットに変更し、動的なメモリ更新と読み取りをサポートしました。これにより、コードの可読性が向上し、新しいスロットの追加が容易になりました。さらに、メモリ内のスロットが見つからない場合には、デフォルトで0.0を返すように修正しました。

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
 use std::io::stdin;
 
 fn main() {
-    let mut memory = Memory {
-        slots: vec![0.0; 10],
-    };
+    let mut memory = Memory::new();
     let mut prev_result: f64 = 0.0;
     for line in stdin().lines() {
         // 1行読み取って空行なら終了
@@ -17,16 +15,16 @@ fn main() {
         // memoryへの書き込み
         let is_memory = tokens[0].starts_with("mem");
         if is_memory && tokens[0].ends_with('+') {
-            add_and_print_memory(&mut memory, tokens[0], prev_result);
+            memory.add_and_print(tokens[0], prev_result);
             continue;
         } else if is_memory && tokens[0].ends_with('-') {
-            add_and_print_memory(&mut memory, tokens[0], -prev_result);
+            memory.add_and_print(tokens[0], -prev_result);
             continue;
         }
 
         // 式の計算
-        let left = eval_token(tokens[0], &memory);
-        let right = eval_token(tokens[2], &memory);
+        let left = memory.eval_token(tokens[0]);
+        let right = memory.eval_token(tokens[2]);
         let result = eval_expression(left, tokens[1], right);
         print_output(result);
         prev_result = result;
@@ -34,28 +32,49 @@ fn main() {
 }
 
 struct Memory {
-    slots: Vec<f64>,
+    slots: Vec<(String, f64)>,
 }
 
-fn add_and_print_memory(memory: &mut Memory, token: &str, prev_result: f64) {
-    let slot_index: usize = token[3..token.len() - 1].parse().unwrap();
-    memory.slots[slot_index] += prev_result; // 自動的に参照外しが行われる
-                                         // (*memories)[slot_index] += prev_result; と同じ
-    print_output(memory.slots[slot_index]);
+impl Memory {
+    fn new() -> Self {
+        Self { slots: vec![] }
+    }
+
+    fn add_and_print(&mut self, token: &str, prev_result: f64) {
+        let slot_name = &token[3..token.len() - 1];
+        // すべてのメモリを探索する
+        for slot in self.slots.iter_mut() {
+            if slot.0 == slot_name {
+                // メモリが見つかったので、値を更新・表示して終了
+                slot.1 += prev_result;
+                print_output(slot.1);
+                return;
+            }
+        }
+        self.slots.push((slot_name.to_string(), prev_result));
+        print_output(prev_result);
+    }
+
+    fn eval_token(&self, token: &str) -> f64 {
+        if token.starts_with("mem") {
+            let slot_name = &token[3..];
+            // すべてのメモリを探索する
+            for slot in &self.slots {
+                if slot.0 == slot_name {
+                    // メモリが見つかったので、値を返す
+                    return slot.1;
+                }
+            }
+            // メモリが見つからなかったので、0.0を返す
+            0.0
+        } else {
+            token.parse().unwrap()
+        }
+    }
 }
 
 fn print_output(value: f64) {
     println!(" => {}", value);
-}
-
-fn eval_token(token: &str, memory: &Memory) -> f64 {
-    if token.starts_with("mem") {
-        let slot_index: usize = token[3..].parse().unwrap();
-        memory.slots[slot_index]
-    } else {
-        // 参照のtoken(&str)をもとに新しいf64の値を生成して返す
-        token.parse().unwrap() // 型を指定しなくていいのは、memoryがf64なので型推論されている
-    }
 }
 
 fn eval_expression(left: f64, operator: &str, right: f64) -> f64 {
